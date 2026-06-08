2638fa4384463708494d76f49162880b3ada9be7
Konkvon <joaopedroorem@gmail.com>
2026-06-08 19:02:28 -0300

    Implementação Docker

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import redis
 import json
 from datetime import datetime
 
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_client = redis.StrictRedis(host="redis", port=6379, db=0)
 
 MAX_HISTORY = 10
 HISTORY_KEY = 'history'
@@ -121,4 +121,4 @@ def clear_history():
         return jsonify({'Erro': str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
